d764c122423e434387fc8f52c5d30f225da60863
karimamaaoui <maaouika07@gmail.com>
2021-11-19 02:54:31 +0100

    first commit

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['ptod' => true, 'test' => true],
 ];
